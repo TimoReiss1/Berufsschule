b571b833a90ff55cb6c275afc32c7fd1578a7390
Auer Daniel <D.Auer@eos-ts.com>
2022-10-28 11:43:26 +0200

    added vegetarisch, nuesse etc to rezept

--- skript.sql
+++ skript.sql
@@ -1,3 +1,4 @@
+\c postgres
 DROP DATABASE krautundrueben2;
 CREATE DATABASE krautundrueben2;
 \c krautundrueben2
@@ -17,7 +18,7 @@ CREATE TABLE KUNDE (
 );
 
 CREATE TABLE ZUTAT(
-                      ZUTATENNR           SERIAL NOT NULL,
+                      ZUTATENNR           INTEGER PRIMARY KEY NOT NULL,
                       BEZEICHNUNG         VARCHAR(50),
                       EINHEIT        	VARCHAR (25),
                       NETTOPREIS	        DECIMAL(10,2),
@@ -29,13 +30,19 @@ CREATE TABLE ZUTAT(
 );
 
 CREATE TABLE REZEPT(
-                       REZEPTNR       INTEGER NOT NULL,
-                       ZUTATENNR      INTEGER NOT NULL
-
+                       REZEPTID       INTEGER PRIMARY KEY NOT NULL,
+                       REZEPTNAME     VARCHAR(20),
+                       ZUTATENNR      INTEGER NOT NULL,
+                           VEGETARISCH    BOOLEAN DEFAULT FALSE,
+                       VEGAN          BOOLEAN DEFAULT FALSE,
+                       NUESSE         BOOLEAN DEFAULT FALSE,
+                       FRUTARISCH     BOOLEAN DEFAULT FALSE,
+                       LOWCARB        BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE BESTELLUNG (
                             BESTELLNR        SERIAL NOT NULL,
+                            BESTELLTESREZEPT INTEGER,
                             KUNDENNR         INTEGER,
                             BESTELLDATUM     DATE,
                             RECHNUNGSBETRAG  DECIMAL(10,2),
@@ -44,26 +51,26 @@ CREATE TABLE BESTELLUNG (
 
 
 CREATE TABLE REZEPTZUTAT(
-                            REZEPTNR INTEGER NOT NULL,
-                            ZUTATENNR INTEGER NOT NULL,
-                            MENGE INTEGER,
-                            PRIMARY KEY (REZEPTNR, ZUTATENNR)
+                            REZEPTNR        INTEGER NOT NULL,
+                            ZUTATENNR       INTEGER NOT NULL,
+                            MENGE           INTEGER,
+                            PRIMARY KEY     (REZEPTNR, ZUTATENNR)
 );
 
 CREATE TABLE BESTELLUNGZUTAT (
                                  BESTELLNR       INTEGER NOT NULL,
                                  ZUTATENNR       INTEGER,
-                                 MENGE     		  INTEGER
+                                 MENGE           INTEGER
 );
 
 CREATE TABLE LIEFERANT (
                            LIEFERANTENNR   INTEGER PRIMARY KEY NOT NULL,
                            LIEFERANTENNAME VARCHAR(50),
                            STRASSE         VARCHAR(50),
-                           HAUSNR			  VARCHAR(6),
+                           HAUSNR          VARCHAR(6),
                            PLZ             VARCHAR(5),
                            ORT             VARCHAR(50),
-                           TELEFON			  VARCHAR(25),
+                           TELEFON         VARCHAR(25),
                            EMAIL           VARCHAR(50)
 );
 
@@ -71,9 +78,6 @@ CREATE TABLE LIEFERANT (
 /***                              Primary Keys                              ***/
 /******************************************************************************/
 
-
-ALTER TABLE ZUTAT ADD PRIMARY KEY (ZUTATENNR);
-ALTER TABLE BESTELLUNGZUTAT ADD PRIMARY KEY (BESTELLNR,ZUTATENNR);
 
 
 /******************************************************************************/
@@ -84,3 +88,6 @@ ALTER TABLE ZUTAT ADD FOREIGN KEY (LIEFERANT) REFERENCES LIEFERANT (LIEFERANTENN
 ALTER TABLE BESTELLUNGZUTAT ADD FOREIGN KEY (BESTELLNR) REFERENCES BESTELLUNG (BESTELLNR);
 ALTER TABLE BESTELLUNG ADD FOREIGN KEY (KUNDENNR) REFERENCES KUNDE (KUNDENNR);
 ALTER TABLE BESTELLUNGZUTAT ADD FOREIGN KEY (ZUTATENNR) REFERENCES ZUTAT (ZUTATENNR);
+ALTER TABLE REZEPTZUTAT ADD FOREIGN KEY (REZEPTNR) REFERENCES REZEPT (REZEPTID);
+ALTER TABLE REZEPTZUTAT ADD FOREIGN KEY (ZUTATENNR) REFERENCES ZUTAT (ZUTATENNR);
+ALTER TABLE BESTELLUNG ADD FOREIGN KEY (BESTELLTESREZEPT) REFERENCES REZEPT(REZEPTID);
