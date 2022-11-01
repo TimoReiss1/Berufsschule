5210a4bddd3ba244a1d47de6eaf73ce5b001c3aa
Auer Daniel <D.Auer@eos-ts.com>
2022-11-01 11:53:33 +0100

    added queries

--- script_queries.sql
+++ script_queries.sql
@@ -1,3 +1,13 @@
 /* join bestellung and bestellung rezept */
 SELECT bestellung.bestellnr, bestellung_rezept.rezeptnr, bestellung_rezept.menge, bestellung.kundennr, bestellung.bestelldatum, bestellung.rechnungsbetrag
 FROM bestellung JOIN bestellung_rezept ON bestellung.bestellnr = bestellung_rezept.bestellnr;
+
+/* join rezept and rezept_zutat */
+SELECT rezept.rezeptnr, rezept.rezeptname, rezept_zutat.zutatennr, rezept_zutat.menge
+FROM rezept JOIN rezept_zutat ON rezept.rezeptnr = rezept_zutat.rezeptnr;
+
+/* join rezept, rezept_zutat and zutat */
+SELECT rezept.rezeptnr, rezept.rezeptname, rezept_zutat.zutatennr, rezept_zutat.menge, zutat.bezeichnung, zutat.nettopreis
+FROM rezept
+JOIN rezept_zutat ON rezept.rezeptnr = rezept_zutat.rezeptnr
+JOIN zutat ON rezept_zutat.zutatennr = zutat.zutatennr;
