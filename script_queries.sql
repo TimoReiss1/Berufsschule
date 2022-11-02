e42c1e63b02479d1bb4b9d68f524a4b511608d5a
Auer Daniel <D.Auer@eos-ts.com>
2022-11-02 12:30:56 +0100

    adjusted queries

--- script_queries.sql
+++ script_queries.sql
@@ -23,7 +23,18 @@ JOIN zutat ON rezept_zutat.zutatennr = zutat.zutatennr
 WHERE zutat.bezeichnung = 'Ei';
 
 /*Berechnung der durchschnittlichen Nährwerte aller Bestellungen eines Kunden*/
-/*ROUND()*/
+/*e.g.*/
+SELECT bestellung.kundennr, SUM(zutat.kalorien * rezept_zutat.menge * bestellung_rezept.menge) as kalorien,
+       /*AVG(kalorien),*/
+       SUM(zutat.kohlenhydrate * rezept_zutat.menge * bestellung_rezept.menge) as kohlenhydrate,
+       SUM(zutat.protein * rezept_zutat.menge * bestellung_rezept.menge) as protein
+FROM bestellung
+         JOIN bestellung_rezept ON bestellung.bestellnr = bestellung_rezept.bestellnr
+         JOIN rezept ON rezept.rezeptnr = bestellung_rezept.rezeptnr
+         JOIN rezept_zutat ON rezept.rezeptnr = rezept_zutat.rezeptnr
+         JOIN zutat ON rezept_zutat.zutatennr = zutat.zutatennr
+where bestellung.kundennr = 2001
+group by bestellung.kundennr;
 
 /*Auswahl aller Zutaten, die bisher keinem Rezept zugeordnet sind*/
 
@@ -44,3 +55,12 @@ SELECT rezept.rezeptnr, rezept.rezeptname, rezept_zutat.zutatennr, rezept_zutat.
 FROM rezept
 JOIN rezept_zutat ON rezept.rezeptnr = rezept_zutat.rezeptnr
 JOIN zutat ON rezept_zutat.zutatennr = zutat.zutatennr;
+
+/* select rezepte with protein, kalorien and kohlenhydrate */
+SELECT rezept.rezeptnr, rezept.rezeptname, SUM(zutat.kalorien * rezept_zutat.menge) as kalorien,
+       SUM(zutat.kohlenhydrate * rezept_zutat.menge) as kohlenhydrate,
+       SUM(zutat.protein * rezept_zutat.menge) as protein
+FROM rezept
+         JOIN rezept_zutat ON rezept.rezeptnr = rezept_zutat.rezeptnr
+         JOIN zutat ON rezept_zutat.zutatennr = zutat.zutatennr
+group by rezept.rezeptnr;
